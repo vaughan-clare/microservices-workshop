62790254a231a121b4f913cdca1fc6d6860eb246
Vaughan Nickerson <vaughan.nickerson@parivedasolutions.com>
2017-11-19 13:55:35 -0500

    Comments

--- dino-facts/lib/index.js
+++ dino-facts/lib/index.js
@@ -1,22 +1,31 @@
 /* eslint-disable no-use-before-define */
-const extend = require('lodash/extend');
 const dinoFacts = require('../mock_data/dino_info');
 
 module.exports = function () {
   const seneca = this;
-  seneca.add({ role: 'dinosaur', cmd: 'get-facts' }, retrieveDinoFacts);
+  seneca.add({ role: 'dinosaur', cmd: 'get-facts' }, directoryHandler);
   return {
     name: 'jurassic-park',
   };
 };
 
-function retrieveDinoFacts(msg, done) {
+function directoryHandler(msg, done) {
   const seneca = this;
-  seneca.log.info('DINOSAUR DIRECTORY', msg);
+  seneca.log.info('Running directory handler', msg.dinosaurName, msg);
+  /*
+    Check the incoming msg for the `dinosaurName` value.
+    If it's not there, handle this with an error message.
+  */
+
   const dinosaurName = msg.dinosaurName;
   if (!dinosaurName || !dinoFacts[dinosaurName]) {
     done({ ok: false, error: `Could not retrive facts with property 'dinosaurName':'${dinosaurName}'` });
     return;
   }
-  done(extend({}, { ok: true }, dinoFacts[dinosaurName]));
+
+  /*
+    Reference the dinoFacts source file for the dinosaur bio by name,
+    return the dino facts via the done() callback.
+  */
+  done(dinoFacts[dinosaurName]);
 }
