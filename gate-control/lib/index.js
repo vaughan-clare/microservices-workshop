76aa0a89627137f6874f8f0555a1213e9253675b
Vaughan Nickerson <vaughan.nickerson@parivedasolutions.com>
2017-11-19 15:01:48 -0500

    Phase 2 Complete code

--- gate-control/lib/index.js
+++ gate-control/lib/index.js
@@ -4,7 +4,6 @@ let gatesOpen = true;
 module.exports = function () {
   const seneca = this;
   seneca.add({ role: 'gates', cmd: 'control' }, controlGates);
-  seneca.add({ role: 'gates', cmd: 'control', verify: true }, controlGatesWithVerification);
   return {
     name: 'jurassic-park',
   };
@@ -15,12 +14,4 @@ function controlGates(msg, done) {
   seneca.log.info('Running gate control', gatesOpen, msg);
   gatesOpen = !gatesOpen;  
   done({ ok: true });
-}
-
-function controlGatesWithVerification(msg, done) {
-  const seneca = this;
-  seneca.log.info('Running gate control with verification', gatesOpen, msg);
-  gatesOpen = !gatesOpen;
-  const gateStatus = gatesOpen ? 'open' : 'closed';
-  done({ gateStatus });
 }
