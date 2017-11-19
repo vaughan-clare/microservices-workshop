832c09d0c11fa840c6a52b3676eb791cafd83ace
Vaughan Nickerson <vaughan.nickerson@parivedasolutions.com>
2017-11-19 14:23:48 -0500

    Add comments

--- dino-facts/lib/index.js
+++ dino-facts/lib/index.js
@@ -1,30 +1,34 @@
 /* eslint-disable no-use-before-define */
-const dinoFacts = require('../mock_data/dino_info');
+const dinoFacts = require('../mock_data/dino_info');    // Data for dinosaur directory
 
+// Setup for Seneca instance
 module.exports = function () {
   const seneca = this;
+
+  // Add actions this service will be listening for here
   seneca.add({ role: 'dinosaur', cmd: 'get-facts' }, directoryHandler);
+
   return {
-    name: 'jurassic-park',
+    name: 'jurassic-park',       // Name of seneca service
   };
 };
 
 function directoryHandler(msg, done) {
   const seneca = this;
   seneca.log.info('Running directory handler', msg.dinosaurName, msg);
-  /*
-    Check the incoming msg for the `dinosaurName` value.
-    If it's not there, handle this with an error message.
-  */
-
   const dinosaurName = msg.dinosaurName;
+
+  /*
+    TODO: Check the incoming msg for the `dinosaurName` value.
+    If it's falsy, handle this with an error message.
+  */
   if (!dinosaurName || !dinoFacts[dinosaurName]) {
     done({ ok: false, error: `Could not retrive facts with property 'dinosaurName':'${dinosaurName}'` });
     return;
   }
 
   /*
-    Reference the dinoFacts source file for the dinosaur bio by name,
+    TODO: Reference the dinoFacts source file for the dinosaur bio by name,
     return the dino facts via the done() callback.
   */
   done(dinoFacts[dinosaurName]);
